452f3a1b8eee696059939098e3135251f80ce3af
saireddychilla <saireddy4286@gmail.com>
2026-03-14 20:02:05 +0530

    American development team

--- src/interviewQuestions/ReverseString.java
+++ src/interviewQuestions/ReverseString.java
@@ -3,13 +3,19 @@ package interviewQuestions;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		
+		int a=5;
 		System.out.println("reverse string using Streams");
 	
 		System.out.println("reverse string using Streams");
 		
 		System.out.println("reverse string using Streams");
+		
 	
+	}
+	public static void add()
+	{
+	int b= 5;
+	System.out.println("American development team");
 	}
 
 }
